4d8a835d440647412790b309132b0e5c8a1d058d
lyt <beefnoodles.cc@gmail.com>
2019-07-02 18:06:01 +0800

    修改启动脚本

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,5 +1,5 @@
-cd /root
-nohup java -jar -Dspring.profiles.active=dev -server -d64 -Xms512m -Xmx512m -Xmn256m -Xss512k  /root/jenkins/workspace/jiwuzao/order/target/order-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
+cd /tmp
+## nohup java -jar -Dspring.profiles.active=dev -server -d64 -Xms512m -Xmx512m -Xmn256m -Xss512k  /root/jenkins/workspace/jiwuzao/order/target/order-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
 nohup java -jar -Dspring.profiles.active=dev -server -d64 -Xms512m -Xmx512m -Xmn256m -Xss512k  /root/jenkins/workspace/jiwuzao/major/target/major-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
 nohup java -jar -Dspring.profiles.active=dev -server -d64 -Xms512m -Xmx512m -Xmn256m -Xss512k  /root/jenkins/workspace/jiwuzao/manager/target/manager-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
 echo "success"
