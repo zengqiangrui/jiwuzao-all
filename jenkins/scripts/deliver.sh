425edaa696924ee0ccfaf91dd8366b99e0e8535d
lyt <i@beefnoodles.cc>
2019-07-02 22:12:45 +0800

    更改路径

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /tmp
 # nohup java -jar -Dspring.profiles.active=dev -server -d64 -Xms512m -Xmx512m -Xmn256m -Xss512k  /root/jenkins/workspace/jiwuzao/order/target/order-0.0.1-SNAPSHOT.jar >/dev/null 2>&1 &
-nohup java -jar -Dspring.profiles.active=dev -server -d64 -Xms512m -Xmx512m -Xmn256m -Xss512k  /root/jenkins/workspace/jiwuzao/major/target/major-0.0.1-SNAPSHOT.jar >/tmp/major-log 2>&1 &
-nohup java -jar -Dspring.profiles.active=dev -server -d64 -Xms512m -Xmx512m -Xmn256m -Xss512k  /root/jenkins/workspace/jiwuzao/manager/target/manager-0.0.1-SNAPSHOT.jar >/tmp/manager-log 2>&1 &
+nohup java -jar -Dspring.profiles.active=dev -server -d64 -Xms512m -Xmx512m -Xmn256m -Xss512k  /var/jenkins_home/workspace/jiwuzao/major/target/major-0.0.1-SNAPSHOT.jar >/var/jenkins_home/logs/major-log 2>&1 &
+nohup java -jar -Dspring.profiles.active=dev -server -d64 -Xms512m -Xmx512m -Xmn256m -Xss512k  /var/jenkins_home/workspace/jiwuzao/manager/target/manager-0.0.1-SNAPSHOT.jar >/var/jenkins_home/logs/manager-log 2>&1 &
 echo "success"
