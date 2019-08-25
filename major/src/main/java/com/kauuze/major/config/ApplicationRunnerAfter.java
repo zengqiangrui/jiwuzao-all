7500c16fc650377a5f9b2f2ff0c75877eeb26194
zengqinagrui <zengqiangrui@163.com>
2019-08-25 13:42:57 +0800

    prod

--- major/src/main/java/com/kauuze/major/config/ApplicationRunnerAfter.java
+++ major/src/main/java/com/kauuze/major/config/ApplicationRunnerAfter.java
@@ -31,23 +31,18 @@ import java.time.LocalDateTime;
 @Component
 @Order(1)
 public class ApplicationRunnerAfter implements ApplicationRunner {
-    private AppVersion updateVersion() {
-        AppVersion appVersion = new AppVersion();
-        appVersion.setVersion("1.0.1").setVersionCode(101).setCreateTime(System.currentTimeMillis()).setStop(false)
-                .setUpdateContent("更新一个版本").setDownloadUrl("http://download.jiwuzao.com/jiwuzao.apk");
-        return appVersion;
-    }
 
     @Override
     public void run(ApplicationArguments applicationArguments) throws Exception {
         if (!ConfigUtil.validCustomEnvironment()) {
             throw new RuntimeException("validCustomEnvironment error!");
         }
-        if (StringUtil.isEq(ConfigUtil.customEnvironment, "prod")) {
-            SystemService bean = SpringContext.getBean(SystemService.class);
-            AppVersion appVersion = bean.createUpdateVersion("1.0.0", 100, "http://download.jiwuzao.com/jiwuzao.apk", "更新");
-            if(appVersion == null) throw new RuntimeException("请检查版本信息和更新环境");
-        }
+
+//        if (StringUtil.isEq(ConfigUtil.customEnvironment, "prod")) {
+//            SystemService bean = SpringContext.getBean(SystemService.class);
+//            AppVersion appVersion = bean.createUpdateVersion("1.0.0", 100, "http://download.jiwuzao.com/jiwuzao.apk", "更新");
+//            if(appVersion == null) throw new RuntimeException("请检查版本信息和更新环境");
+//        }
 
         AppVersionRepository appVersionRepository = SpringContext.getBean(AppVersionRepository.class);
         SystemNoticeRepository systemNoticeRepository = SpringContext.getBean(SystemNoticeRepository.class);
@@ -60,6 +55,7 @@ public class ApplicationRunnerAfter implements ApplicationRunner {
             SystemGoods systemGoods = new SystemGoods(null, SystemGoodsNameEnum.deposit, SystemGoodsNameEnum.deposit.name, null, new BigDecimal("0.01"));
             systemGoodsRepository.insert(systemGoods);
         }
+
         if (appVersionRepository.findAll().size() == 0) {
             AppVersion appVersion = new AppVersion();
             appVersion.setCreateTime(System.currentTimeMillis());
@@ -70,46 +66,46 @@ public class ApplicationRunnerAfter implements ApplicationRunner {
             appVersionRepository.insert(appVersion);
         }
 
-
         if (systemNoticeRepository.findAll().size() == 0) {
             SystemNotice systemNotice = new SystemNotice();
             systemNoticeRepository.insert(systemNotice);
         }
-        UserRepository userRepository = SpringContext.getBean(UserRepository.class);
-        UserTokenRepository userTokenRepository = SpringContext.getBean(UserTokenRepository.class);
-        if (userRepository.findAll().size() == 0) {
-            UserBasicService userBasicService = SpringContext.getBean(UserBasicService.class);
-            Integer msCode = userBasicService.sendSms("10000000000");
-            Integer msCode2 = userBasicService.sendSms("10000000001");
-            Integer msCode3 = userBasicService.sendSms("10000000002");
-            String accessToken = (String) userBasicService.register("10000000000", "qq1501643251", "kauuze", msCode).getData();
-            String accessToken2 = (String) userBasicService.register("10000000001", "qq1501643251", "kauuze2", msCode2).getData();
-            String accessToken3 = (String) userBasicService.register("10000000002", "qq1501643251", "kauuze3", msCode3).getData();
-            UserToken userToken = userTokenRepository.findByAccessToken(accessToken);
-            UserToken userToken2 = userTokenRepository.findByAccessToken(accessToken2);
-            UserToken userToken3 = userTokenRepository.findByAccessToken(accessToken3);
-            userToken.setBackRole(BackRoleEnum.root);
-            userToken2.setBackRole(BackRoleEnum.cms);
-            userToken3.setVip(true);
-            userToken3.setVipEndTime(DateTimeUtil.covertMill(LocalDateTime.now().plusYears(100)));
-            userTokenRepository.save(userToken);
-            userTokenRepository.save(userToken2);
-            userTokenRepository.save(userToken3);
-            userTokenRepository.save(userToken3);
-            SmsRepository smsRepository = SpringContext.getBean(SmsRepository.class);
-            Sms sms = smsRepository.findByPhone("10000000000");
-            Sms sms2 = smsRepository.findByPhone("10000000001");
-            Sms sms3 = smsRepository.findByPhone("10000000002");
-            sms.setCode(123123);
-            sms.setOverTime(2111111111111L);
-            smsRepository.save(sms);
-            sms2.setCode(123123);
-            sms2.setOverTime(2111111111111L);
-            smsRepository.save(sms2);
-            sms3.setCode(123123);
-            sms3.setOverTime(2111111111111L);
-            smsRepository.save(sms3);
-        }
+
+//        UserRepository userRepository = SpringContext.getBean(UserRepository.class);
+//        UserTokenRepository userTokenRepository = SpringContext.getBean(UserTokenRepository.class);
+//        if (userRepository.findAll().size() == 0) {
+//            UserBasicService userBasicService = SpringContext.getBean(UserBasicService.class);
+//            Integer msCode = userBasicService.sendSms("10000000000");
+//            Integer msCode2 = userBasicService.sendSms("10000000001");
+//            Integer msCode3 = userBasicService.sendSms("10000000002");
+//            String accessToken = (String) userBasicService.register("10000000000", "qq1501643251", "kauuze", msCode).getData();
+//            String accessToken2 = (String) userBasicService.register("10000000001", "qq1501643251", "kauuze2", msCode2).getData();
+//            String accessToken3 = (String) userBasicService.register("10000000002", "qq1501643251", "kauuze3", msCode3).getData();
+//            UserToken userToken = userTokenRepository.findByAccessToken(accessToken);
+//            UserToken userToken2 = userTokenRepository.findByAccessToken(accessToken2);
+//            UserToken userToken3 = userTokenRepository.findByAccessToken(accessToken3);
+//            userToken.setBackRole(BackRoleEnum.root);
+//            userToken2.setBackRole(BackRoleEnum.cms);
+//            userToken3.setVip(true);
+//            userToken3.setVipEndTime(DateTimeUtil.covertMill(LocalDateTime.now().plusYears(100)));
+//            userTokenRepository.save(userToken);
+//            userTokenRepository.save(userToken2);
+//            userTokenRepository.save(userToken3);
+//            userTokenRepository.save(userToken3);
+//            SmsRepository smsRepository = SpringContext.getBean(SmsRepository.class);
+//            Sms sms = smsRepository.findByPhone("10000000000");
+//            Sms sms2 = smsRepository.findByPhone("10000000001");
+//            Sms sms3 = smsRepository.findByPhone("10000000002");
+//            sms.setCode(123123);
+//            sms.setOverTime(2111111111111L);
+//            smsRepository.save(sms);
+//            sms2.setCode(123123);
+//            sms2.setOverTime(2111111111111L);
+//            smsRepository.save(sms2);
+//            sms3.setCode(123123);
+//            sms3.setOverTime(2111111111111L);
+//            smsRepository.save(sms3);
+//        }
     }
 
 }
--- major/src/test/java/com/kauuze/major/test/ExpressRepositoryTest.java
+++ major/src/test/java/com/kauuze/major/test/ExpressRepositoryTest.java
@@ -1,11 +1,19 @@
 package com.kauuze.major.test;
 
+import com.jiwuzao.common.domain.enumType.ExpressEnum;
+import com.jiwuzao.common.domain.mongo.entity.Express;
 import com.kauuze.major.domain.mongo.repository.ExpressRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.imageio.stream.FileCacheImageInputStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 @RunWith(SpringRunner.class)
@@ -25,7 +33,7 @@ public class ExpressRepositoryTest {
 
 
     @Test
-    public void show(){
+    public void show() {
         String code = "feDex_gj";
 
         String str = "FEDEX_GJ";
@@ -33,8 +41,22 @@ public class ExpressRepositoryTest {
     }
 
     @Test
-    public void showT(){
-
+    public void showT() throws IOException {
+//        File f = new File("C://express.txt");
+//        FileInputStream fileInputStream = new FileInputStream(f);
+//        InputStreamReader isr = new InputStreamReader(fileInputStream);
+//        BufferedReader br = new BufferedReader(isr);
+//        List<Express> list = new ArrayList<>();
+//        String line = "";
+//        while ((line=br.readLine())!=null) {
+//            System.out.println(line);
+//            if(!line.trim().equals("")){
+//                String[] split = line.split("\\s");
+//                Express express = new Express().setName(split[0]).setCode(split[1]).setExpressType(ExpressEnum.COMMON);
+//                list.add(express);
+//            }
+//        }
+//        expressRepository.saveAll(list);
     }
 
 }
